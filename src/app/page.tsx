f27a66020f4e2234fed21e7459da776a2edf845b
DikhaAG <dikhaagsfrdy@gmail.com>
2025-06-12 20:21:31 +0700

    change: buku panduan kmipn vi, header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-yellow-300">
+    <div className="min-h-screen bg-yellow-300 pt-20">
       {/* <header className="fixed top-0 left-0 right-0 z-50 bg-purple-400 */}
       {/*   text-white p-6 border-b-8 border-black"> */}
       {/*   <div className="container mx-auto"> */}
@@ -13,28 +13,28 @@ export default function Home() {
       {/*   </div> */}
       {/* </header> */}
       {/* <div className="w-auto items-center max-w-fit absolute border"> */}
-      <div className="flex m-auto w-fit gap-4 pt-10 cursor-default">
-        <h2 style={{ textShadow: "4px 4px black" }} className="sm:hidden w-fit text-3xl md:text-8xl font-black uppercase mb-4 text-white">
+      <div className="flex m-auto w-fit px-4 py-2 gap-4 cursor-default bg-white rounded-xl shadow-[8px_8px_0px_0px_#000]">
+        <h2 className="sm:hidden w-fit text-3xl md:text-8xl font-black uppercase mb-4 text-black mt-0.5">
           Road To
         </h2>
-        <h2 style={{ textShadow: "6px 6px black" }} className="hidden sm:flex w-fit text-4xl md:text-8xl font-black uppercase mb-4 text-white">
+        <h2 className="hidden sm:flex w-fit text-4xl md:text-8xl font-black uppercase mb-4 text-black mt-0.5">
           Road To
         </h2>
         <h2 style={{ textShadow: "6px 6px black" }} className="hidden sm:flex w-fit text-4xl md:text-8xl font-black uppercase mb-4 text-purple-400">
           KMIPN VII
         </h2>
-        <h2 style={{ textShadow: "4px 4px black" }} className="sm:hidden w-fit text-3xl md:text-8xl font-black uppercase mb-4 text-purple-400">
+        <h2 style={{ textShadow: "3px 3px black" }} className="sm:hidden w-fit text-3xl md:text-8xl font-black uppercase mb-4 text-purple-400">
           KMIPN VII
         </h2>
       </div>
-      <div className="m-auto w-fit text-1xl  md:text-2xl cursor-default bg-white font-black rounded-full px-6 shadow-[8px_8px_0px_0px_#000]">
+      <div className="m-auto w-fit text-1xl -translate-y-4  md:text-2xl cursor-default bg-white font-black rounded-full px-6 shadow-[8px_8px_0px_0px_#000]">
         Teknik Komputer 2025
       </div>
 
       {/* </div> */}
       {/* Body */}
-      <main className="flex-1 flex flex-col items-center justify-center min-h-[calc(100vh-200px)] p-6 pt-32">
-        <div className="w-full max-w-md space-y-8 pt-10">
+      <main className="flex-1 flex flex-col items-center justify-center min-h-[calc(100vh-200px)] p-6">
+        <div className="w-full max-w-md space-y-8">
           {/*<div className="text-center mb-12">
             <h2 className="text-3xl md:text-4xl font-black uppercase mb-4 text-black">CHOOSE YOUR</h2>
             <h3 className="text-2xl md:text-3xl font-black uppercase text-red-500">ADVENTURE</h3>
@@ -47,7 +47,7 @@ export default function Home() {
             </Button>
             <Button asChild
               className="w-full bg-red-500 text-white font-black text-xl md:text-2xl uppercase py-6 px-8 border-4 border-black shadow-[8px_8px_0px_0px_#000] hover:bg-red-700 hover:shadow-[4px_4px_0px_0px_#000] hover:translate-x-1 hover:translate-y-1 transition-all duration-150 active:shadow-[2px_2px_0px_0px_#000] active:translate-x-2 active:translate-y-2">
-              <Link href="https://drive.google.com/file/d/1q4DxjZT_A3stX_mX_umdczLSoiTu61n_/view?usp=drive_link">Buku Panduan KMIPN VII</Link>
+              <Link href="https://drive.google.com/file/d/1q4DxjZT_A3stX_mX_umdczLSoiTu61n_/view?usp=drive_link">Buku Panduan KMIPN VI</Link>
             </Button>
 
             <Button asChild className="w-full bg-blue-500 hover:bg-blue-700 text-white font-black text-xl md:text-2xl uppercase py-6 px-8 border-4 border-black shadow-[8px_8px_0px_0px_#000] hover:shadow-[4px_4px_0px_0px_#000] hover:translate-x-1 hover:translate-y-1 transition-all duration-150 active:shadow-[2px_2px_0px_0px_#000] active:translate-x-2 active:translate-y-2">
